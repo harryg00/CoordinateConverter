124a729292f27a2249358b74afd8490d34d34ee1
harryg00 <67504429+harryg00@users.noreply.github.com>
2026-04-06 22:24:43 +0100

    Stopped it only using localhost

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,7 @@
     const latitude = document.getElementById("latitude").value;
     const longitude = document.getElementById("longitude").value;
 
-    const result = await fetch(`https://localhost:7070/Home/ClosestCentreline?latitude=${latitude}&longitude=${longitude}`).then(response => response.text());
+    const result = await fetch(`/Home/ClosestCentreline?latitude=${latitude}&longitude=${longitude}`).then(response => response.text());
     console.log(result);
     document.getElementById("result").textContent = result;
 }
